6e4d42b8fbb97e99d4ff3c82c30a42073103fea3
aizaz aziz <aizaz.azzi@yahoo.com>
2022-08-08 17:14:53 +0500

    Initial commit

---
+++ src/Controllers/InspirationController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Aizaz\Inspire\Controllers;
+
+use Illuminate\Http\Request;
+use Aizaz\Inspire\Inspire;
+
+class InspirationController
+{
+    public function __invoke(Inspire $inspire) {
+        $quote = $inspire->justDoIt();
+
+        return view('inspire::index', compact('quote'));
+    }
+}
---
+++ src/Inspire.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+namespace Aizaz\Inspire;
+
+
+
+use Illuminate\Support\Facades\Http;
+
+class Inspire {
+    public function justDoIt() {
+        $response = Http::get('https://inspiration.goprogram.ai/');
+
+        return $response['quote'] . ' -' . $response['author'];
+    }
+}
